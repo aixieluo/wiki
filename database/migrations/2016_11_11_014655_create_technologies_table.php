7713ec1d4c2241753d1333668022dafed1fc5387
wohai <wohaiainixxy@qq.com>
2017-05-14 23:25:51 +0800

    xxy

--- database/migrations/2016_11_11_014655_create_technologies_table.php
+++ database/migrations/2016_11_11_014655_create_technologies_table.php
@@ -18,6 +18,9 @@ class CreateTechnologiesTable extends Migration
             $table->integer('technology_type_id')->unsigned()->comment('科技类型外键');
             $table->string('name')->comment('科技名称');
             $table->integer('rank')->comment('阶级');
+            $table->integer('row')->nullable()->comment('行数');
+            $table->integer('column')->nullable()->comment('列数');
+            $table->string('string')->comment('科技树位置');
             $table->foreign('technology_type_id')->references('id')->on('technology_types')->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
